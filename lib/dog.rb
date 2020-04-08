6046e32df9073cba936e6bc059b729e122c2d19c
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-08 21:42:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  
+  def name=(whats_the_name)
+    @dogs_name = whats_the_name
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  def bark
+    puts  "woof!"
+  end
+
+end
